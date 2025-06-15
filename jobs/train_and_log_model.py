10e69a308eda7aab978bcfa787b5999de2401e65
Gabriele Folchi <gabriele.folchi@cloudera.com>
2025-06-15 08:42:48 +0000

    Initial commit of local project

---
+++ jobs/train_and_log_model.py
@@ -0,0 +1,112 @@
+import boto3
+import os
+import pandas as pd
+from io import StringIO
+from datetime import datetime, timezone, timedelta
+
+import mlflow
+import mlflow.sklearn
+from sklearn.linear_model import ElasticNet
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+import numpy as np
+from mlflow.models import infer_signature
+
+aws_access_key_id = os.environ["AWS_ACCESS_KEY"]
+aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
+
+print(f">>>> Read AWS credentials to access rawdata bucket")
+
+# S3 configuration
+# bucket = 'gab-bucket-removeme'
+# key = 'rawdata/winequality-red.csv'  # Update with the actual key in your bucket
+
+bucket = os.environ["S3_RAWDATA_BUCKET"]
+key = os.environ["S3_RAWDATA_KEY"]  # Update with the actual key in your bucket
+
+print(f">>>> Reading rawdata csv from s3 bucket ")
+print(f">>>>>>>> Bucket: {bucket} ....")
+print(f">>>>>>>> Bucket Key: {key} ....")
+
+
+# Create S3 client with credentials
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key
+)
+
+# Get object metadata
+head = s3.head_object(Bucket=bucket, Key=key)
+last_modified = head['LastModified']
+
+# Check if object was modified within the last hour
+now = datetime.now(timezone.utc)
+if (now - last_modified) <= timedelta(hours=1):
+    # Read object from S3
+    response = s3.get_object(Bucket=bucket, Key=key)
+    body = response['Body'].read().decode('utf-8')
+
+    # Load CSV into DataFrame
+    df = pd.read_csv(StringIO(body), sep=';')
+
+else:
+    print("Object is older than 1 hour. Skipping.")
+
+# Display data
+df.head()
+
+
+# 4. Split data
+X = df.drop("quality", axis=1)
+y = df["quality"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_test.head()
+
+# 5. Set up experiment and start run
+mlflow.set_experiment("wine_quality_experiment")
+
+# override sys.argv with arguments conveyed via JOB_ARGUMENTS variable:
+import sys, os, shlex
+sys.argv = ["script"] + shlex.split(os.environ.get("JOB_ARGUMENTS", ""))
+print(sys.argv)
+
+with mlflow.start_run(run_name="elasticnet_wine-job1008"):
+
+    # Log hyperparameters
+    alpha = 0.5
+    l1_ratio = 0.5
+    mlflow.log_param("alpha", alpha)
+    mlflow.log_param("l1_ratio", l1_ratio)
+
+    # Train model
+    model = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+    model.fit(X_train, y_train)
+
+    # Evaluate and log metrics
+    preds = model.predict(X_test)
+    mse = mean_squared_error(y_test, preds)
+    rmse = np.sqrt(mse)
+    mae = mean_absolute_error(y_test, preds)
+    r2 = r2_score(y_test, preds)
+
+
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("mae", mae)
+    mlflow.log_metric("r2", r2)
+
+    # Set a tag that we can use to remind ourselves what this run was for
+    mlflow.set_tag("Training Info", "Basic model for wine quality - from job")
+
+    # Infer the model signature
+    signature = infer_signature(X_train, model.predict(X_train))
+    
+    # 6. Log and register model
+    model_info = mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="elasticnet_model",
+        signature=signature,
+        input_example=X_test,
+        registered_model_name="ElasticNetWineModel"
+    )
---
+++ onnx-elasticnet_wine_quality_client.py
@@ -0,0 +1,65 @@
+
+#!pip install open-inference-openapi
+from open_inference.openapi.client import OpenInferenceClient, InferenceRequest
+import httpx
+import json
+import os
+
+
+# If running from workbench use /tmp/jwt. Otherwise provide your CDP_TOKEN
+API_KEY = json.load(open("/tmp/jwt"))["access_token"]
+
+
+BASE_URL = 'https://caii-inference.emerging.dp5i-5vkq.cloudera.site/namespaces/serving-default/endpoints/elasticnetwine2141onnx'
+MODEL_NAME = 'a00d-9ysj-zlk6-hu3t'
+headers = {
+	'Authorization': 'Bearer ' + API_KEY,
+	'Content-Type': 'application/json'
+}
+
+httpx_client = httpx.Client(headers=headers)
+client = OpenInferenceClient(base_url=BASE_URL, httpx_client=httpx_client)
+
+# Check that the server is live, and it has the model loaded
+client.check_server_readiness()
+metadata = client.read_model_metadata(MODEL_NAME)
+metadata_str = json.dumps(json.loads(metadata.json()), indent=2)
+print(metadata_str)
+
+
+# Load JSON file (list of dicts)
+with open("sample_inputs/X_test_first_record.json", "r") as f:
+    records = json.load(f)  # e.g. [ {dict}, {dict}, ... ]
+
+# Extract columns in order from the first record's keys
+columns = list(records[0].keys())
+
+# Build data values as list of lists in column order
+data_values = [
+    [record[col] for col in columns] 
+    for record in records
+]
+
+# Prepare inputs payload
+inputs_test = [
+    {
+        "name": "input",
+        "datatype": "FP32",
+        "shape": [len(data_values), len(columns)],
+        "data": data_values
+    }
+]
+
+print(json.dumps(inputs_test, indent=2))
+
+
+# Make an inference request
+pred = client.model_infer(
+	MODEL_NAME,
+	request=InferenceRequest(
+		inputs=inputs_test
+	)
+)
+
+json_resp_str = json.dumps(json.loads(pred.json()), indent=2)
+print(json_resp_str)
